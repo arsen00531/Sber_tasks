708ae46f8217d781febc5690d749ea967cf346ba
arsen00531 <arsendag505@gmail.com>
2024-04-21 11:48:57 +0300

    complete first task

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,25 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+const ignore = ['groupId', 'service', 'formatSize', 'ca']
 
+export function translate(encoded, translations) {
+    const result = []
 
+    encoded.forEach((encod) => {
+        const object = {}
 
-// console.log(decoded)
+        for (let encodedKey of Object.keys(encod)) {
+            object[encodedKey] = encod[encodedKey]
+
+            if (!ignore.includes(encodedKey) && !isNaN(encod[encodedKey]) && encod[encodedKey] !== null) {
+                object[encodedKey] = translations[encod[encodedKey]] || ""
+            }
+        }
+        
+        result.push(object)
+    })
+
+    return result
+}
+
+console.log(translate(encoded, translations))
